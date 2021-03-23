ce81f2da3ab70a98b32c962b52a28ccc206e4e90
Jiangxue Han <juviahan@163.com>
2021-03-23 01:11:43 -0400

    Camera working

--- app/src/main/java/com/example/InstagramClone/ParseApplication.java
+++ app/src/main/java/com/example/InstagramClone/ParseApplication.java
@@ -3,13 +3,14 @@ package com.example.InstagramClone;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("QAF7ptTWE4dtQEttY4SOxbL7enSxFJnHlZ8dQqKf")
                 .clientKey("jpNsHMKULCvOhlEgV6t6xJvb1PgpkIcOqRUfpw4v")
